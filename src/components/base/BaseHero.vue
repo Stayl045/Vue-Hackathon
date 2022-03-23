22fe7f425042bff266d17d36523836e620d15b1e
cosmob3 <bryanlvelasco@hotmail.com>
2022-03-23 11:02:35 -0600

    added BaseHero

---
+++ src/components/base/BaseHero.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>
+    Helping people makes the world a better place through quality software
+  </h1>
+  <div>
+    <button>Read the blog</button>
+    <button>Take a Course</button>
+  </div>
+  <div>
+    <img src="#" alt="Hero image" />
+  </div>
+</template>
+
+<script setup></script>
